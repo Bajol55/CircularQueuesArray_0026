820c2a6e67dc93eb42645e68a6aa6ad4e430483e
Bajol55 <devarafadil35@gmail.com>
2026-05-28 16:32:09 +0700

    menyempurnakan traversal cetak untuk kondisi susunan indeks linear

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -97,4 +97,13 @@ public:
 
             cout << endl;
         }
-       
+        else
+        {
+            // jika FRONT <= REAR, iterasi dari posisi FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+          
